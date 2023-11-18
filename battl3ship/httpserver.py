eb2bb6e2552b4bbec07241e43e4947a8917d0931
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2023-11-18 19:06:30 +0100

    Quick fix to allow playing in 2023 and python 3

--- battl3ship/httpserver.py
+++ battl3ship/httpserver.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 #
 # This file is part of the Battl3ship game.
-# 
+#
 #     Battl3ship is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     Battl3ship is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with Battl3ship.  If not, see <http://www.gnu.org/licenses/>.
 """HTTP server for the Py3Sink game.
@@ -180,5 +180,9 @@ class HTTPGameServer:
 
 
 if __name__ == '__main__':
+    import asyncio
+    from tornado.platform.asyncio import AnyThreadEventLoopPolicy
+    asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+
     gamer_server = HTTPGameServer(port=default_http_port)
     gamer_server.serve_forever()
